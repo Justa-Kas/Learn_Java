af3ed9e6ba442a376c0efe44025c928d0253d6dd
Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-02-13 14:55:12 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.print("Hello World. ");
+        System.out.println("How do you do?");
+        System.out.println("My name is Kasean.");
+    }
+
+}
