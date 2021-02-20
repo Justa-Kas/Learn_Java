1c33fe4112c9131f8e5f69d15ae54a919c32e4b4
Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-02-20 15:09:07 -0500

    Did some variables. multiplied, added, and printed out the variables too. Solid day.

--- src/HelloWorldo/HelloWorld.java
+++ src/HelloWorldo/HelloWorld.java
@@ -1,3 +1,5 @@
+package HelloWorldo;
+
 public class HelloWorld {
     public static void main(String[] args) {
 
---
+++ src/Variables/MyFirstVariable.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class MyFirstVariable {
+    public static void main(String[] args) {
+        int myFirstVariable = 5;
+        int solidNumber = 20;
+        int mySecondVariable = 10;
+        int myThirdVariable = 5;
+        int myTotal = myThirdVariable + mySecondVariable;
+        int calculation = (myFirstVariable + mySecondVariable) * myTotal;
+        System.out.println(myFirstVariable);
+        System.out.print("20 ");
+        System.out.print(solidNumber + " ");
+        System.out.print(mySecondVariable *2 + " ");
+        System.out.print(10+10+" ");
+        System.out.println(mySecondVariable + mySecondVariable);
+        System.out.println(myTotal);
+        System.out.println(calculation);
+    }
+}
