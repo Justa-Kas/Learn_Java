6b16b988c0b2e7fc13cc3632378e8d0e6b838725
Just Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-03-30 17:50:01 -0400

    did a speed conversion using methods and the Math.round operator?

--- src/Methods/SpeedConverter.java
+++ src/Methods/SpeedConverter.java
@@ -1,10 +1,31 @@
 package Methods;
 
 public class SpeedConverter {
+    public static void main(String[] args) {
+        printConversion(100.54);
+        System.out.println(toMilesPerHour(15));
+    }
     public static long toMilesPerHour(double kilometersPerHour){
+        double miles;
         if(kilometersPerHour<0){
             return -1;
-        }else if(kilometersPerHour)
+        }else if(kilometersPerHour>=0){
+            miles= kilometersPerHour*0.62137119;
+            return Math.round(miles);
+        }else{
+            return -1;
+        }
+    }
+    public static void printConversion(double kilometersPerHour){
+        double miles;
+        if(kilometersPerHour<0){
+            System.out.println("Invalid value.");
+        }else if(kilometersPerHour>=0){
+            miles= kilometersPerHour*0.62137119;
+            System.out.println(kilometersPerHour+" km/h = "+Math.round(miles)+" mi/h");
+        }else{
+            System.out.println("-1");
+        }
     }
 }
 
