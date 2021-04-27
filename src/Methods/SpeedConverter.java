a02a741f39ea2e51696b257ab009236fd993c4e8
Just Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-04-27 10:15:26 -0400

    Update SpeedConverter.java

--- src/Methods/SpeedConverter.java
+++ src/Methods/SpeedConverter.java
@@ -2,8 +2,8 @@ package Methods;
 
 public class SpeedConverter {
     public static void main(String[] args) {
-        printConversion(100.54);
-        System.out.println(toMilesPerHour(15));
+        printConversion(540);
+        System.out.println(toMilesPerHour(540));
     }
     public static long toMilesPerHour(double kilometersPerHour){
         double miles;
@@ -16,15 +16,14 @@ public class SpeedConverter {
             return -1;
         }
     }
+
     public static void printConversion(double kilometersPerHour){
-        double miles;
         if(kilometersPerHour<0){
             System.out.println("Invalid value.");
         }else if(kilometersPerHour>=0){
-            miles= kilometersPerHour*0.62137119;
-            System.out.println(kilometersPerHour+" km/h = "+Math.round(miles)+" mi/h");
+            System.out.println(kilometersPerHour+" km/h = "+toMilesPerHour(kilometersPerHour)+" mi/h");
         }else{
-            System.out.println("-1");
+            System.out.println("Error.");
         }
     }
 }
