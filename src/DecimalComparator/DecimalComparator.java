fa080fe5ba2dea878d957546ad0daf01da7616b2
Just Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-04-27 11:17:42 -0400

    Update DecimalComparator.java

--- src/DecimalComparator/DecimalComparator.java
+++ src/DecimalComparator/DecimalComparator.java
@@ -1,5 +1,13 @@
 package DecimalComparator;
 
 public class DecimalComparator {
-public static boolean areEqualByThreeDecimal
+public static boolean areEqualByThreeDecimalPlaces(double param1, double param2){
+    int casted1= (int) param1*1000;
+    int casted2= (int) param2*1000;
+    if(casted1==casted2){
+        return true;
+    }else{
+        return false;
+    }
+}
 }
