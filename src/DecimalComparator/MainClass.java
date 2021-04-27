b92c907715808d40998e071626b0df3dbbc01d92
Just Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-04-27 11:17:54 -0400

    Update MainClass.java

--- src/DecimalComparator/MainClass.java
+++ src/DecimalComparator/MainClass.java
@@ -2,6 +2,10 @@ package DecimalComparator;
 
 public class MainClass {
     public static void main(String[] args) {
-
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(34.237,34.237));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(34.2373,34.237));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(36.237,34.237));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(34.237,34.2376));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(34.237,39.237));
     }
 }
